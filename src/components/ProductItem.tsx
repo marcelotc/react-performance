6027dcdb7329ba36e80651259a8e5c29744f3a8e
Marcelo Teixeira Cortes <marceloteixeiracortes@gmail.com>
2021-05-29 19:06:53 -0300

    Exemplo de aplicação

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface ProductItemProps {
+    product: {
+        id: number;
+        price: number;
+        title: string;
+    }
+}
+
+export default function ProductItem({ product }: ProductItemProps) {
+    return (
+        <div>
+            {product.title} - <strong>{product.price}</strong>
+        </div>
+    )
+}
